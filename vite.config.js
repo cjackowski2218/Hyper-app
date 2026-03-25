ba4c47332d4ed46bbdcefda7a49a266bc042f752
cjackowski2218 <c.jackowski22@gmail.com>
2026-03-25 19:55:52 -0400

    Add vite-plugin-pwa for proper iOS PWA support

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['icons/*.png'],
+      manifest: {
+        name: 'HYPER',
+        short_name: 'HYPER',
+        description: 'Hypertrophy training log',
+        background_color: '#0d1117',
+        theme_color: '#0d1117',
+        display: 'standalone',
+        orientation: 'portrait',
+        start_url: '/',
+        icons: [
+          { src: '/icons/icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/icons/icon-512.png', sizes: '512x512', type: 'image/png' },
+          { src: '/icons/icon-1024.png', sizes: '1024x1024', type: 'image/png' }
+        ]
+      }
+    })
+  ]
 })
