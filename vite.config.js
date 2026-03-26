e64aa58fe0c9945cb3dd3ef09f8247f98e2b58d1
cjackowski2218 <c.jackowski22@gmail.com>
2026-03-25 20:08:43 -0400

    Revert vite config, fix nav safe area padding

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['icons/*.png'],
-      manifest: {
-        name: 'HYPER',
-        short_name: 'HYPER',
-        description: 'Hypertrophy training log',
-        background_color: '#0d1117',
-        theme_color: '#0d1117',
-        display: 'standalone',
-        orientation: 'portrait',
-        start_url: '/',
-        icons: [
-          { src: '/icons/icon-192.png', sizes: '192x192', type: 'image/png' },
-          { src: '/icons/icon-512.png', sizes: '512x512', type: 'image/png' },
-          { src: '/icons/icon-1024.png', sizes: '1024x1024', type: 'image/png' }
-        ]
-      }
-    })
-  ]
+  plugins: [react()],
 })
